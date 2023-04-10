90e64cfa7a2d554e6d6a51fd518eda572935bcc6
Daryl Lin <daryllin@Daryls-MacBook-Pro.local>
2023-04-10 11:24:29 -0700

    Suporting homepage

--- front_end_proxy_api_trigger.js
+++ front_end_proxy_api_trigger.js
@@ -6,11 +6,17 @@ async function logJSONData(url) {
         //trigger utag.view
         var processedURL = new URL(url)
         var pathName = processedURL.pathname
-        console.log(pathName)
-        //pathFragments supports homepage, one level, 2 levels
-        var pathFragments1 = await pathName.split("/").length > 1 ? pathName.split("/")[1] : null; 
-        var pathFragments2 = await pathName.split("/").length > 1 ? pathName.split("/")[2] : null;
-        
+
+        //if only 1 or less backslash, means that it is homepage
+        if(pathName.split("1") <= 1){
+            var pathFragments1 = "homepage"
+            var pathFragments2 = "homepage"
+        } else {
+            //if more than 1 back slash; then we have valid page_type / valid page_name
+            var pathFragments1 =  pathName.split("/").length > 1 ? pathName.split("/")[1] : null; 
+            var pathFragments2 =  pathName.split("/").length > 1 ? pathName.split("/")[2] : null;
+        }
+
         var utag_data = {
             page_type: !pathFragments1 ? null : pathFragments1,
             page_name: !pathFragments2 ? null : pathFragments2
